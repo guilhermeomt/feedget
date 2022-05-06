93734d302c65d9e6c4a18505665416199b81e3b5
Guilherme <guilhermet213@gmail.com>
2022-05-06 09:57:49 -0300

    feat: consume api with axios

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,7 +1,9 @@
 import { ArrowLeft } from "phosphor-react";
 import { FormEvent, useState } from "react";
+import { api } from "../../../lib/api";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
+import { Loading } from "../Loading";
 import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackTypeStepProps {
@@ -17,12 +19,22 @@ export function FeedbackContentStep({
 }: FeedbackTypeStepProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null);
   const [comment, setComment] = useState("");
+  const [isLoadingFeedback, setIsLoadingFeedback] = useState(false);
 
   const feedbackTypeInfo = feedbackTypes[feedbackType];
 
-  function handleSubmitFeedback(event: FormEvent<HTMLFormElement>) {
+  async function handleSubmitFeedback(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
+    setIsLoadingFeedback(true);
+
+    const result = await api.post("/feedbacks", {
+      type: feedbackType,
+      comment,
+      screenshot,
+    });
+
+    setIsLoadingFeedback(false);
     onFeedbackSent();
   }
 
@@ -64,10 +76,10 @@ export function FeedbackContentStep({
 
           <button
             type="submit"
-            disabled={comment.length === 0}
+            disabled={comment.length === 0 || isLoadingFeedback}
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
           >
-            Enviar feedback
+            {isLoadingFeedback ? <Loading /> : "Enviar feedback"}
           </button>
         </footer>
       </form>
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,11 +1,11 @@
 import react from "@vitejs/plugin-react";
-import path from "node:path";
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
   },
   plugins: [react()],
 });
